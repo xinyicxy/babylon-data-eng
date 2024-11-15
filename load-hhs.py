0e060f2d3700b98754d1822d7325357f03e346ed
xinyikAmy <112516468+xinyikAmy@users.noreply.github.com>
2024-11-15 14:13:24 -0500

    Update load-hhs.py comment

--- load-hhs.py
+++ load-hhs.py
@@ -1,4 +1,4 @@
-"""Upload HHS dataset"""
+"""Import HHS dataset"""
 import sys
 import pandas as pd
 import psycopg
@@ -9,7 +9,6 @@ import credentials
 file_path = str(sys.argv[1])
 df = pd.read_csv(file_path)
 
-
 # Unique hospital id
 df = df[df['hospital_pk'].str.match(r'^\d{6}$')]
 df = df.dropna(subset=['hospital_pk', 'collection_week'])
@@ -17,8 +16,7 @@ df = df.drop_duplicates(subset=['hospital_pk'])
 # Collection date
 df['collection_week'] = pd.to_datetime(df['collection_week'])
 
-
-# Numbers
+# Columns for weekly table
 cols = ['all_adult_hospital_beds_7_day_avg',
         'all_adult_hospital_inpatient_bed_occupied_7_day_avg',
         'all_pediatric_inpatient_beds_7_day_avg',
@@ -29,11 +27,10 @@ cols = ['all_adult_hospital_beds_7_day_avg',
         'staffed_icu_adult_patients_confirmed_covid_7_day_avg']
 df[cols] = df[cols].where(df[cols] >= 0, None)
 
-
-# Select columns by names
+# Weekly table copy
 df1 = df[['hospital_pk', 'collection_week'] + cols].copy()
 
-
+# Columns for demo table
 cols_demo = [
         'hospital_name',
         'address',
@@ -43,15 +40,15 @@ cols_demo = [
         'geocoded_hospital_address'
 ]
 
-
-# Select columns by names
+# Demo table copy
 df2 = df[['hospital_pk'] + cols_demo].copy()
 
 
+"""Extracting Latitude and Longitude information"""
 def extract_lat_long(geo_address):
-    if geo_address == "NA":  # Check if the value is "NA"
+    if geo_address == "NA":
         return pd.Series([None, None])
-    elif pd.notna(geo_address):  # Proceed only if geo_address is not NaN
+    elif pd.notna(geo_address): 
         match = re.match(r'POINT \(([-+]?\d+\.\d+)\s+([-+]?\d+\.\d+)\)',
                          geo_address)
         if match:
@@ -65,22 +62,26 @@ def extract_lat_long(geo_address):
 df2[['latitude', 'longitude']] = df2['geocoded_hospital_address'].apply(
     extract_lat_long)
 
-
+# Rename the fips column
 df2 = df2.rename(columns={'fips_code': 'fips'})
 
-
+# Read data into tables
 conn = psycopg.connect(
    host="pinniped.postgres.database.azure.com",
    dbname=credentials.DB_USER,
    user=credentials.DB_USER,
    password=credentials.DB_PASSWORD
 )
+
+# Insert into demo table
 cur_demo = conn.cursor()
+# Covert the dataset to list of tuples for batch insert
 demo = [(row.hospital_pk, row.hospital_name, row.state,
          row.address, row.zip,
          row.fips, row.latitude, row.longitude,) for row in df2.itertuples(
              index=False)]
 try:
+    # Update the demographics information when seeing the same hospital id again
     cur_demo.executemany(
         "INSERT INTO demo"
         "(id, name, state, address, zip, fips, latitude, longitude)"
@@ -95,16 +96,15 @@ try:
         "longitude = EXCLUDED.longitude;",
         demo
     )
+    # Print the number of rows inserted
     rowcount = cur_demo.rowcount
-    print(rowcount, " rows have been inserted into database \"demo\"")
+    print(rowcount, " rows have been inserted into database quality")
 
 except Exception as err:
     rowcount = cur_demo.rowcount
     print(err, " at row ", rowcount)
-else:
-    conn.commit()
 
-
+# Insert into weekly table
 cur_weekly = conn.cursor()
 weekly = [(row.hospital_pk,
            row.collection_week,
@@ -122,17 +122,17 @@ try:
     cur_weekly.executemany(
         "INSERT INTO weekly"
         "(hospital_id, collection_week, adult_beds, adult_bed_occupied, \
-            pediatric_beds, pediatric_bed_occupied, icu_beds, \
-                icu_bed_occupied, beds_covid, icu_covid)"
+            pediatric_beds, pediatric_bed_occupied, icu_beds, icu_bed_occupied, \
+                beds_covid, icu_covid)"
         "VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)",
         weekly
     )
     rowcount = cur_weekly.rowcount
-    print(rowcount, " rows have been inserted into database \"weekly\"")
+    print(rowcount, " rows have been inserted into database quality")
 
 except Exception as err:
     rowcount = cur_weekly.rowcount
     print(err, " at row ", rowcount)
-else:
-    conn.commit()
+
+conn.commit()
 conn.close()
